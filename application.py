aed1e319c47c246376bb6daaa90e671f874d576a
AkshayKumar977 <2006akshaykumar@gmail.com>
2025-07-14 08:16:07 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-ridge_model = pickle.load(open('models/ridge.pkl','rb'))
-standard_scaler = pickle.load(open('models/scaler.pkl','rb'))
+ridge_model = pickle.load(open('ridge.pkl','rb'))
+standard_scaler = pickle.load(open('scaler.pkl','rb'))
 @app.route("/")
 def index():
     return render_template("index.html")
